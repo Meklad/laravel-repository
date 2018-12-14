bab0726f4c32a1dfe233d7fafaeb413775a5e5ad
Meklad <ahmed.meklad.news@gmail.com>
2018-12-14 03:26:40 +0200

    #comment Add Service Provider to the Package:LaravelTaggyServiceProvider #time 0 #type ADD

---
+++ src/LaravelTaggyServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Owllog\LaravelTaggy;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTaggyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
