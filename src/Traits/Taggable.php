3c16445adbcd7fa8e8439340133892c8f84605d5
Meklad <ahmed.meklad.news@gmail.com>
2018-12-15 22:43:19 +0200

    #comment Modify the taggble trait to handel the tags if (array-object-collection) and sync to database #time 1h #type MOD

--- src/Traits/Taggable.php
+++ src/Traits/Taggable.php
@@ -2,7 +2,9 @@
 
 namespace Owllog\LaravelTaggy\Traits;
 
+use Illuminate\Support\Collection;
 use Owllog\LaravelTaggy\Models\Tag;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * @author Ahmed Meklad <ahmed.meklad.news@gmail>
@@ -17,5 +19,49 @@ trait Taggable
     public function tags()
     {
         return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    public function tag($tags)
+    {
+        $this->addTags($this->getWorkableTags($tags));
+    }
+
+    private function addTags(Collection $tags)
+    {
+        $sync = $this->tags()->syncWithoutDetaching(
+            $tags->pluck('id')->toArray()
+        );
+    }
+
+    private function getWorkableTags($tags)
+    {
+        if(is_array($tags)) {
+            return $this->getTagModel($tags);
+        }
+
+        if($tags instanceof Model) {
+            return $this->getTagModel([$tags->slug]);
+        }
+
+        return $this->filterTagsCollection($tags);
+    }
+
+    private function filterTagsCollection(Collection $tags)
+    {
+        return $tags->filter(function($tag) {
+            return $tag instanceof Model;
+        });
+    }
+
+    private function getTagModel(array $tags)
+    {
+        return Tag::whereIn('slug', $this->normailzeTagName($tags))->get();
+    }
+
+    private function normailzeTagName(array $tags)
+    {
+        return array_map(function($tag) {
+            return str_slug($tag);
+        }, $tags);
     }
 }
